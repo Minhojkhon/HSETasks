410061bf13f51fec2d51e72ead52da96d4f096e1
Minhojkhon <minhojkhon@icloud.com>
2023-01-19 02:43:05 +0300

    Want to delete?

--- CAOS/call_scanf_printf.c
+++
@@ -1,6 +0,0 @@
-int read_multiply_add(int a1, int a2, int a3);
-
-int main() {
-
-    read_multiply_add(1, 2, 3);
-}
--- CAOS/simple_printf.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void print_4_5();
-
-int main() {
-    print_4_5();
-}
